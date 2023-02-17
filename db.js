49fd6a790c1775b335f0fda17506d516d8b516d8
A-Rifkan <a.rifkan2019@gmail.com>
2023-02-17 09:59:49 +0000

    Added models and set up database

---
+++ db.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize");
+const path = require('path');
+
+// TODO - connect to db via sequelize
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: path.join(__dirname, "db.sqlite")
+})
+module.exports = {
+    sequelize
+};
